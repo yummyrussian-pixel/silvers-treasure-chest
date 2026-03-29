ac5915cb13d04b10dbbcd803f25433113322f513
yummyrussian-pixel <yummyrussian@gmail.com>
2026-03-30 03:39:47 +1300

    Create loading.js

---
+++ app/loading.js
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <div style={{ 
+      minHeight: '100vh', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      backgroundColor: '#0f0f0f',
+      color: '#C6A85B'
+    }}>
+      <p>Loading Silvers Treasure Chest...</p>
+    </div>
+  );
+}
